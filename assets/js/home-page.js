86d871c3f8eaabef5d92951a326377eee0ed173b
Rex Yu <yu19910513@gmail.com>
2021-05-04 14:14:19 -0700

    passport function done. please revise

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -3,7 +3,8 @@
   $('.eventBtn').on("click", function(event) {
     var stateName = event.target.value;
     console.log(event.target.text);
-    localStorage.setItem('stateVisited', stateName)
+    localStorage.setItem('stateVisited', stateName);
+    localStorage.setItem(stateName, stateName);
     window.location.href = 'page2.html';
   })
 
@@ -11,5 +12,69 @@
   $('a').on("click", function(event) {
     var stateName = event.target.text;
     console.log(event.target.text);
-    localStorage.setItem('stateVisited', stateName)
+    localStorage.setItem('stateVisited', stateName);
+    localStorage.setItem(stateName, stateName);
   });
+
+
+  var stateArray = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+    ];
+
+  for (let i = 0; i < stateArray.length; i++) {
+
+    if (stateArray[i] == localStorage.getItem(stateArray[i])) {
+      $('.passport').append($('<div>').text(stateArray[i]))
+    }
+
+
+  }
